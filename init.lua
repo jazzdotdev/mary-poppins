3c2db7d6dc5ecbd0b441fb3f4a1e461c961c0154
sharafzaman <sharafzaz121@gmail.com>
2018-12-10 04:25:16 +0530

    test mutliple packages

--- init.lua
+++ init.lua
@@ -1,6 +1,15 @@
 local scl_file = fs.read_file("import.scl")
 local table = scl.to_table(scl_file)
 
-os.execute("git clone " .. table.packages[1])
+function sync()
+	os.execute("mkdir .mp && cd .mp")
+	
+	for _, url in pairs(table.packages) do 
+		os.execute("git clone " .. url)
+	end
+
+end
+
+sync()
 
 return 0
