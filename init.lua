36e014b129a7185a133fed970110e0526748b6dd
sharafzaman <sharafzaz121@gmail.com>
2018-12-10 07:02:33 +0530

    update functionality added;
    bug fix

--- init.lua
+++ init.lua
@@ -3,30 +3,19 @@ local table = scl.to_table(scl_file)
 
 function sync()
 	for rep_name, location in pairs(table.imports) do
-		local dir_name = get_dir_name(location)
-		if dir_name ~= "" then 
-			-- TODO check if directory is already present 
-			os.execute("mkdir " .. dir_name .. " && cd " .. dir_name)
-			os.execute("git clone " .. table[rep_name]["url"])
-			os.execute("cd -")
-		else
-			os.execute("git clone" .. table[rep_name]["url"])
-		end
+		os.execute("git clone " .. table[rep_name]["url"] .. 
+		" " .. location)
 	end 
 end
 
-function get_dir_name(str) 
-	local i = string.len(str) - 1
-	while i >= 0 do
-		if string.sub(str, i, i) == '/' then
-			return string.sub(str, 0, i)
-		end
-		i = i - 1
+function update() 
+	for rep_name, location in pairs(table.imports) do
+		os.execute("cd " .. location .. " && " ..
+		" git pull -v ")
 	end
-	return ""
 end
 
-
 sync()
+update()
 
 return 0
