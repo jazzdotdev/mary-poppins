80cf9c6b125f655c6500385b54432f30e99b7399
sharafzaman <sharafzaz121@gmail.com>
2018-12-12 04:17:35 +0530

    recursive update

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
+DEFAULT_PATH = "./"
+
 function sync(path)
-	local scl_file = fs.read_file(path .. "/import.scl")
-	local table = scl.to_table(scl_file)
+	local table = get_table_from(path) 
 
 	-- download the repo
 	for rep_name, location in pairs(table.imports) do
@@ -10,7 +11,6 @@ function sync(path)
 		if table[rep_name]["recursive"] == true then
 			sync(location)
 		end
-
 	end
 end
 
@@ -18,17 +18,26 @@ function fetch(url, location)
 	git.clone(url, location)
 end
 
-function update() 
+function update(path) 
+	local table = get_table_from(path)
 	for rep_name, location in pairs(table.imports) do
 		git.pull(location)
 
+		if table[rep_name]["recursive"] == true then
+			update(location)
+		end
 	end
 end
 
+function get_table_from(path) 
+	local scl_file = fs.read_file(path .. "/import.scl")
+	return scl.to_table(scl_file) 
+end
+
 if os.getenv("MP_ACTION") == "sync" or os.getenv("MP_ACTION") == "" then
-	sync("./") 
+	sync(DEFAULT_PATH) 
 elseif os.getenv("MP_ACTION") == "update" then
-	update()
+	update(DEFAULT_PATH)
 else
 	print("Invalid Option\n" ..
 		  "usage: ./mp [ACTIONS]")
