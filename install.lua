7b9d050ac6d43182b2079dc99e0737b8053d4ee0
Sharaf Zaman <sharafzaz121@gmail.com>
2018-12-16 03:44:45 +0530

    improved install.lua

--- install.lua
+++ install.lua
@@ -2,13 +2,12 @@ local function install(name)
 
 	local packages_table = get_table_from(REPOSITORY_HOME, "packages.scl")
 
-	for package_name, values in pairs(packages_table) do 
-		if (package_name == name) then
-			fetch(values[URL], package_name, REPOSITORY_HOME)
-			return
-		end
+	if packages_table[name] ~= nil then
+		fetch(packages_table[name][URL], name, REPOSITORY_HOME)
+		return
+	else 
+		print("Package not found")
 	end
-	print("Package not found")
 end
 
 return install
