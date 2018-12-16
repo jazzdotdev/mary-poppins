2261f65bf646a955079ef1004d83408059d35113
Sharaf Zaman <sharafzaz121@gmail.com>
2018-12-16 06:54:40 +0530

    executable symlinks

--- install.lua
+++ install.lua
@@ -2,12 +2,15 @@ local function install(name)
 
 	local packages_table = get_table_from(REPOSITORY_HOME, MANIFEST_FILE)
 
-	if packages_table[name] ~= nil then
-		fetch(packages_table[name], name, INSTALL_HOME)
-		return
-	else 
+	if packages_table[name] == nil then
 		print("Package not found")
+		return
 	end
+
+	fetch(packages_table[name], name, INSTALL_HOME) 
+	-- TODO use bindings
+	os.execute(string.format("ln -s %s/%s/init.lua /usr/bin/%s ", INSTALL_HOME, name, name))
+	
 end
 
 return install
