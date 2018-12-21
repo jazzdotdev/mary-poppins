a6c1bfb6b8b4aacb452eb0094d7988d79a56457b
Sharaf Zaman <sharafzaz121@gmail.com>
2018-12-22 01:09:15 +0530

    fixes #18: mp crash on first use
    bug fix: `refresh.lua` clone if directory doesn't exist

--- refresh.lua
+++ refresh.lua
@@ -1,8 +1,12 @@
 -- get file from remote server
 -- udpates existing file 
 local function refresh() 
-  git.reset(REPOSITORY_HOME, "origin/master", "hard")
-  git.pull(REPOSITORY_HOME, "origin", "master")
+  if fs.exists(REPOSITORY_HOME) then
+    git.reset(REPOSITORY_HOME, "origin/master", "hard")
+    git.pull(REPOSITORY_HOME, "origin", "master")
+  else
+    fetch(REPOSITORY_URL, "", REPOSITORY_HOME)
+  end
 end
 
 return refresh
