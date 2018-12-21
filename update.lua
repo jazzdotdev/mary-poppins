a1562c58fbf1a17b3df7cd12697832a2c7b1a38c
Sharaf Zaman <sharafzaz121@gmail.com>
2018-12-22 00:38:50 +0530

    updated for new `pull` bindings

--- update.lua
+++ update.lua
@@ -2,7 +2,7 @@ local function update(path)
   path = path or DEFAULT_PATH
   local table = get_table_from(path)
   for rep_name, location in pairs(table.imports) do
-    git.pull(DEFAULT_SAVE_DIRECTORY .. rep_name)
+    git.pull(DEFAULT_SAVE_DIRECTORY .. rep_name, "origin", "master")
 
     if table[rep_name][RECURSIVE] == true then
       update(DEFAULT_SAVE_DIRECTORY .. rep_name)
