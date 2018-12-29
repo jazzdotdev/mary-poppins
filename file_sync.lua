62d47a889d12b8fe2577a9757953dcb06648aed0
Sharaf Zaman <sharafzaz121@gmail.com>
2018-12-30 05:23:41 +0530

    switched to bindings

--- file_sync.lua
+++ file_sync.lua
@@ -1,5 +1,4 @@
 -- syncs the files from `.mp/` to their respective as per import.scl
--- TODO use bindings rather than `os.execute`
 file_sync = {}
 
 function pluck(from_path, rep_name, to_path, file_name)
@@ -7,7 +6,8 @@ function pluck(from_path, rep_name, to_path, file_name)
   local rep_loc = get_rep_path(rep_name)
   local dest_path = get_dest_path(from_path, to_path)
 
-  os.execute(string.format("mkdir -p %s && cp %s/%s %s", dest_path, rep_loc, file_name, dest_path))
+  fs.create_dir(dest_path, true)
+  fs.copy_dir(rep_loc .. file_name, dest_path)
 end
 
 function export(from_path, rep_name, to_path, export_directory)
@@ -15,16 +15,15 @@ function export(from_path, rep_name, to_path, export_directory)
   local export_dir_path = rep_loc .. "/" .. export_directory
   local dest_path = get_dest_path(from_path, to_path)
 
-  os.execute(
-    string.format("mkdir -p %s && cp %s/* %s -r", dest_path, export_dir_path, dest_path)
-  )
+  fs.create_dir(dest_path, true)
+  fs.copy_dir(export_dir_path, dest_path)
 end
 
 function full_copy(from_path, rep_name, to_path)
-    local rep_loc = get_rep_path(rep_name)
-  local dest_path = get_dest_path(from_path, to_path)
-  -- todo: escape shell arguments similar to `gut`
-  os.execute(string.format("mkdir -p %s && cp %s/* %s -r", dest_path, rep_loc, dest_path))
+  local rep_loc = get_rep_path(rep_name)
+  local dest_path = get_dest_path(from_path, to_path) 
+  fs.create_dir(dest_path, true)
+  fs.copy_dir(rep_loc, dest_path)
 end
 
 function get_rep_path(rep_name)
