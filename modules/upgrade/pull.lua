9c17f076f69221235d26fd39d4d41a5fa1d679ce
Sharaf Zaman <sharafzaz121@gmail.com>
2019-01-12 03:47:14 +0530

    Bug fix
    git pull in android

--- modules/upgrade/pull.lua
+++ modules/upgrade/pull.lua
@@ -1,7 +1,7 @@
 -- todo better names and organization?
 return function(rep_path)
   if torchbear.os == "android" then
-    os.execute("git pull " .. rep_path .. " origin master")
+    os.execute("git -C " .. rep_path .. " pull")
   else
     git.pull(rep_path, "origin", "master")
   end
