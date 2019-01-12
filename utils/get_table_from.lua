d497e2f95ddfcbb5a5d23cb6305a6d3d1f81e292
Sharaf Zaman <sharafzaz121@gmail.com>
2019-01-12 21:59:15 +0530

    exit if scl file not found

--- utils/get_table_from.lua
+++ utils/get_table_from.lua
@@ -1,12 +1,18 @@
 function get_table_from(path, name)
-  name = name or "/import.scl"
-  -- todo: fix path handling
+	name = name or "/import.scl" 
 
   if torchbear.os == "windows" then
-    path = cwd .. path
+    path = fs.join(cwd, path)
+	end
+	
+	scl_path = fs.join(path, name)
+
+	if not fs.exists(scl_path) then
+		print("Error import.scl file not found: " .. scl_path)
+		os.exit(1)
   end
 
-  local scl_file = fs.read_file(path .. name)
+  local scl_file = fs.read_file(scl_path)
 
   return scl.to_table(scl_file)
 end
